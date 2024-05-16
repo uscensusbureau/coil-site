9497e87de1e05f0d65f9537a3a3093b4cf1e6166
Sadie <19538219+sadiejay@users.noreply.github.com>
2024-05-15 21:39:35 -0400

    Updates cypress

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+
+module.exports = {
+  e2e: {
+    // We've imported your old cypress plugins here.
+    // You may want to clean this up later by importing these.
+    setupNodeEvents(on, config) {
+      return require("./cypress/plugins/index.js")(on, config);
+    },
+    baseUrl: "http://localhost:4000",
+  },
+
+};
---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,69 @@
+const headers = [
+  "coil-navbar-container",
+  "main-content",
+  "coil-logo",
+  "our-work-section",
+  "featured-projects-list",
+  "project-item-statventures",
+  "project-item-data-usability",
+  "project-item-top",
+  "project-card-group",
+  "our-values",
+  "capabilities",
+  "collaborate-section",
+  "footer-info",
+];
+
+describe('Homepage test', () => {
+  it('contains ids for all projects and section titles', () => {
+    cy.viewport(1024, 768);
+    cy.visit('/');
+    headers.forEach( header => cy.get(`#${header}`));
+  })
+})
+
+describe('Nav Menus', () => {
+  context('USWDS desktop spec', () => {
+    beforeEach(() => {
+      // run these test as if in a USWDS defined desktop browser
+      cy.viewport(1024, 720);
+      cy.visit('/');
+    })
+
+    it('displays full header', () =>{
+      cy.get(".usa-header--basic .usa-nav").should('be.visible');
+      cy.get('.usa-navbar').should('not.be.visible');
+    })
+  })
+
+  context("USWDS tablet spec", () => {
+    beforeEach(() => {
+      // run tests in an USWDS defined tablet sized viewport
+      cy.viewport(640, 1024);
+      cy.visit('/');
+    })
+
+    // it('')
+  });
+
+  context("USWDS mobile spec", () => {
+    beforeEach(() => {
+      // run tests in an USWDS defined mobile sized viewport
+      cy.viewport(320, 480);
+      cy.visit('/');
+    })
+    it('displays mobile hamburger menu', () => {
+      cy.get(".usa-header--basic .usa-nav").should("not.be.visible");
+      cy.get(".usa-navbar").should("be.visible");
+    })
+
+    it('displays mobile menu on click', () => {
+      cy.get(".usa-header--basic .usa-nav").should(".not.be.visible");
+      cy.get(".usa-navbar").should("be.visible").find("button.usa-menu-btn").click();
+
+      cy.get("nav.usa-nav.is-visible").should("be.visible")
+    })
+  })
+
+
+})
